daa815178315e58f5e141ec50a9ce9693774e70e
Kevin Pedroza G <kpedroza@condorlabs.io>
2022-07-11 16:44:06 -0500

    Use new picture of Hello Kitty

--- template-app/src/App.js
+++ template-app/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import kitty from './kitty-picture.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={kitty} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
